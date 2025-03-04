212d266383e2c5bbcf2ce2a324ca0bd93d2c12c9
Legiang2203 <lechaugiang22@gmail.com>
2025-03-05 01:20:38 +0700

    fixx ui/ux for screen users list, edit account, edit profile

--- settings/create-account.php
+++ settings/create-account.php
@@ -44,91 +44,79 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add User</title>
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <title>Edit Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../src/css/root.css">
-    <link rel="stylesheet" href="../src/css/add.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../src/css/edit-account.css">
 </head>
 <body>
+<?php include '../includes/header.php'; ?>
+    <div class="container">
+        
+    <div class="text-center mb-3">
+    <div class="profile-image-wrapper">
+        <img class="profile-image"
+            src="../src/images/profile-image/<?php echo !empty($user['profile_picture']) ? $user['profile_picture'] : 'default.png'; ?>?t=<?php echo time(); ?>"
+            alt="Profile Image">
+    </div>
+    <h2 class="profile-name"><?php echo htmlspecialchars($user['username'] ?? ''); ?></h2>
+</div>
 
-    <?php include '../includes/header.php'; ?>
-    <div class="justify-center">
-        <div class="add-container">
-            <h1>Add new user</h1>
-            <form id="wordForm" method="post" enctype="multipart/form-data">
-                <div class="form-group">
-                    <div class="input-wrapper">
-                        <span class="icon">
-                            <i class="fa-solid fa-pen-ruler"></i>
-                        </span>
-                        <input type="text" class="form-control" id="first_name" name="first_name" required>
-                        <label for="first_name">First Name</label>
-                    </div>
+        <form method="post" enctype="multipart/form-data">
+            <div class="form-group">
+                <label>Username:</label>
+                <input type="text" class="form-control" id="username" name="username" value="" required>
+
+
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>First Name:</label>
+                    <input type="text" class="form-control" id="first_name" name="first_name" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>Last Name:</label>
+                    <input type="text" class="form-control" id="last_name" name="last_name" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label>Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+
+            </div>
+            <div class="form-group">
+                <label for="profile_picture">Profile Picture</label>
+                <div class="custom-file">
+                    <input type="file" id="profile_picture" name="profile_picture" class="custom-file-input" hidden>
+                    <label for="profile_picture" class="custom-file-label">Picture</label>
                 </div>
 
-                <div class="form-group">
-                    <div class="input-wrapper">
-                        <span class="icon">
-                            <i class="fa-solid fa-pen-ruler"></i>
-                        </span>
-                        <input type="text" class="form-control" id="last_name" name="last_name" required>
-                        <label for="last_name">Last Name</label>
-                    </div>
-                </div>
+            </div>
+            <div class="form-group">
+                <label>New Password:</label>
+                <input type="password" class="form-control" id="password" name="password" value="" required>
 
-                <div class="form-group">
-                    <div class="input-wrapper">
-                        <span class="icon">
-                            <i class="fa-solid fa-pen-ruler"></i>
-                        </span>
-                        <input type="email" class="form-control" id="email" name="email" required>
-                        <label for="email">Email<span>*</span></label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="input-wrapper">
-                        <span class="icon">
-                            <i class="fa-solid fa-pen-ruler"></i>
-                        </span>
-                        <input type="text" class="form-control" id="username" name="username" required>
-                        <label for="username"><span>*</span>Username</label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="input-wrapper">
-                        <span class="icon">
-                            <i class="fa-solid fa-pen-ruler"></i>
-                        </span>
-                        <input type="password" class="form-control" id="password" name="password" required>
-                        <label for="password"><span>*</span>Password</label>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="input-wrapper">
-                        <span class="icon">
-                            <i class="fa-solid fa-pen-ruler"></i>
-                        </span>
-                        <input type="file" class="form-control-file" id="profile_picture" name="profile_picture">
-                        <label for="profile_picture">Profile Picture</label>
-                    </div>
-                </div>
-
-                <div class="form-group">
+            </div>
+            <div class="form-group">
                     <button type="submit">Add user</button>
                 </div>
-
-            </form>
-        </div>
+        </form>
     </div>
     <?php include '../includes/footer.php'; ?>
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script>
+        document.getElementById('profile_picture').addEventListener('change', function(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    document.querySelector('.profile-image').src = e.target.result;
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
